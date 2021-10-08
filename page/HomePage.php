65d36d6064cd1ade1f4698b560dfce25e1c3913e
Crystallizee <benidictus.tf@yahoo.com>
2021-10-09 04:10:05 +0700

    Home & List

---
+++ page/HomePage.php
@@ -0,0 +1,155 @@
+<?php
+    include '../component/navbar.php'
+?>
+    
+    <div class="content">
+      <!-- Body -->
+      <div class="container">
+        <!-- Most Popular -->
+
+        <div class="mostPopular">
+          <h2>Most Popular</h2>
+          <hr />
+          <!-- Book -->
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          
+          <!-- End of Popular -->
+        </div>
+
+        <!-- New Novel -->
+        <div class="newNovel">
+          <h2>New Novel</h2>
+          <hr />
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer bg-dark text-light">
+        <div class="container">
+        <div class="row align-items-start">
+          <div class="col text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book">
+                <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+                <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg
+            >
+            <br>
+            <label for="logo">NovelReader</label>
+          </div>
+          <div class="col">
+            <ul>
+                <li>About</li>
+                <li>Home</li>
+                <li>Agreement</li>
+                <li>zzzzzzzzz</li>
+            </ul>
+          </div>
+          <div class="col text-center">
+            <p>©2021 NovelReader</p>
+          </div>
+        </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- my JS -->
+    <script src="script.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+    
