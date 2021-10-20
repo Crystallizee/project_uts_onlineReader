f44b89ce942d0ac44f44dcd2b83d866a44643e9c
Axell MW <axellwyvern08@gmail.com>
2021-10-20 14:40:41 +0700

    uts

---
+++ component/navbar.php
@@ -0,0 +1,68 @@
+<?=
+'<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- my css -->
+    <link rel="stylesheet" href="../css/style.css" />
+    <title>Home</title>
+  </head>
+  <body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="../page/HomePage.php">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book">
+          <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+          <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg
+      ></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../page/homePage.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Series</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Genre</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../page/listPage.php">List</a>
+          </li>
+          <li>
+            <form class="d-flex">
+              <input class="form-control me-3 ms-4 col-5" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </li>
+        </ul>
+        <!-- if else login -->
+        <i class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"
+            ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle></svg
+          ></a>
+          <ul class="dropdown-menu nav-item" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Account</a></li>
+            <li><a class="dropdown-item" href="#">My Book</a></li>
+            <li><a class="dropdown-item" href="#">Log out</a></li>
+          </ul>
+        </i>
+      </div>
+    </div>
+  </nav>
+
+  <!-- End of Nav -->
+'
+?>
---
+++ page/HomePage.php
@@ -0,0 +1,144 @@
+<?php
+    include '../component/navbar.php'
+?>
+    <div class="content">
+      <!-- Body -->
+      <div class="container">
+        <!-- Most Popular -->
+
+        <div class="mostPopular">
+          <h2>Most Popular</h2>
+          <hr />
+          <!-- Book -->
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          
+          <!-- End of Popular -->
+        </div>
+
+        <!-- New Novel -->
+        <div class="newNovel">
+          <h2>New Novel</h2>
+          <hr />
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+          <div class="card" style="width: 180px;">
+            <img src="https://th.bing.com/th/id/OIP.PmbWzLbCZWDYD2fZDbUg1AHaLW?w=186&h=286&c=7&r=0&o=5&dpr=1.25&pid=1.7" class="card-img-top" alt="...">
+            <div class="card-body" style="list-style-type: none;">
+                <h5 class="card-title">judul</h5>
+                <p class="card-text">
+                    <p id="author">Author</p>
+                    <p id="Date_Publish">Date Publish</p>
+                    <a href="#" class="btn btn-primary">Read</a>
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer bg-dark text-light">
+        <div class="container">
+        <div class="row align-items-start">
+          <div class="col text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book">
+                <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+                <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg
+            >
+            <br>
+            <label for="logo">NovelReader</label>
+          </div>
+          <div class="col">
+            <ul>
+                <li>About</li>
+                <li>Home</li>
+                <li>Agreement</li>
+                <li>zzzzzzzzz</li>
+            </ul>
+          </div>
+          <div class="col text-center">
+            <p>©2021 NovelReader</p>
+          </div>
+        </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php
+    include '../component/footer.php'
+    ?>
+    
